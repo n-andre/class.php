dba8e0f6e5aa3d6d27e8f32ea250cf0ddd0b94fe
ANDREANSYAH N <andre@ANDREANSYAHnoMacBook-Pro.local>
2022-05-24 01:21:18 +0900

    first commit

---
+++ class.php
@@ -0,0 +1,31 @@
+<?php
+
+# class作成
+class Car {
+    static $class_name = "Car";
+
+    function init(){
+        $this->name = "";
+    }
+
+    public function show() {
+        print($this->name. "\n");
+    }
+}
+
+# Carクラスのインスタンスを作成する
+$car = new Car();
+
+#Carのインスタンス変数nameにセダンという文字列を格納する
+$car->name = "セダン";
+
+#Carメソッドであるshow()を実行する
+$car->show();
+#ターミナルにはセダンと表示される
+
+#Carクラス内にあるクラス変数$class_nameをターミナルに表示する
+print(Car::$class_name);
+
+#ターミナルにはCarと表示される
+
+?>
